f2d89b5db0717662eef4591b42e46c3021f12487
tianmingchuang <15275307115@163.com>
2019-07-12 11:33:11 +0800

    aa

---
+++ app/Http/Admin/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\http\Index\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    public function aaa()
+    {
+        return view();
+    }
+}
