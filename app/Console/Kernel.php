16ff1c0345216266093fba3face8a73926ed0c8e
tianmingchuang <15275307115@163.com>
2019-08-27 16:49:32 +0800

    aa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,53 +38,53 @@ class Kernel extends ConsoleKernel
 //            return;
 //            $info = file_get_contents('http://www.tianmingchuang.com/youjia');
 //            $info = json_decode($info,1);
-            $info = file_get_contents('http://www.tianmingchuang.com/youjia');
-            $info = json_decode($info,1)['result'];
-//            dd($info);
-            foreach($info as $v){
-//            dump($v);
-                if ($redis->exists($v['city'])){
-//                dd($v);
-//                unset($v['city'],$v['b90'],$v['0h']);
-
-                    $info1 = json_decode($redis->get($v['city']),1);
-//                dump($info1);
-//                unset($v['city'],$v['b90'],$v['0h']);
-                    foreach($info1 as $vv){
-                        foreach($v as $k=>$vi){
-//                    dump($vi);
-                            if($vi != $info1[$k]){
-//                            dump(11);
-                                unset($v['city'],$v['b90'],$v['0h']);
-                                $date = '';
-                                foreach($v as $k=>$vi){
-//                        dump($vi);
-//                        dump($k);
-                                    $date .= $k.': 每升'.$vi.'元'."\n";
-                                }
-                                $openid = $app->user->list($nextOpenId = null)['data']['openid'];
-//                            dd($openid);
-                                foreach($openid as $vc){
-                                    $app->template_message->send([
-                                        'touser' => $v['city'],
-                                        'template_id' => 's_YzwGdD1NrLBTStf4D_qD88Sqa5OG8oZ3M8cK2QVSs',
-//                                    'url' => 'https://easywechat.org',
-                                        'data' => [
-                                            'first' => $v['city'],
-                                            'keyword1' => $date,
-                                        ],
-                                    ]);
-                                }
-                            }
-                        }
-
-                    }
-
-
-                }
-            }
+//            $info = file_get_contents('http://www.tianmingchuang.com/youjia');
+//            $info = json_decode($info,1)['result'];
+////            dd($info);
+//            foreach($info as $v){
+////            dump($v);
+//                if ($redis->exists($v['city'])){
+////                dd($v);
+////                unset($v['city'],$v['b90'],$v['0h']);
+//
+//                    $info1 = json_decode($redis->get($v['city']),1);
+////                dump($info1);
+////                unset($v['city'],$v['b90'],$v['0h']);
+//                    foreach($info1 as $vv){
+//                        foreach($v as $k=>$vi){
+////                    dump($vi);
+//                            if($vi != $info1[$k]){
+////                            dump(11);
+//                                unset($v['city'],$v['b90'],$v['0h']);
+//                                $date = '';
+//                                foreach($v as $k=>$vi){
+////                        dump($vi);
+////                        dump($k);
+//                                    $date .= $k.': 每升'.$vi.'元'."\n";
+//                                }
+//                                $openid = $app->user->list($nextOpenId = null)['data']['openid'];
+////                            dd($openid);
+//                                foreach($openid as $vc){
+//                                    $app->template_message->send([
+//                                        'touser' => $v['city'],
+//                                        'template_id' => 's_YzwGdD1NrLBTStf4D_qD88Sqa5OG8oZ3M8cK2QVSs',
+////                                    'url' => 'https://easywechat.org',
+//                                        'data' => [
+//                                            'first' => $v['city'],
+//                                            'keyword1' => $date,
+//                                        ],
+//                                    ]);
+//                                }
+//                            }
+//                        }
+//
+//                    }
+//
+//
+//                }
+//            }
             //与redis里的数据做对比油价是否有变动
-        })->daily();
+        })->everyMinute();
     }
 
     /**
